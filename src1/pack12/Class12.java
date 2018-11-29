c06056ad41a16f2556e57eba6c449adb7a4c38f4
subramanya knp <subramanyaknp@gmail.com>
2018-11-29 21:57:20 +0530

    commited with stash1

--- src1/pack12/Class12.java
+++ src1/pack12/Class12.java
@@ -2,6 +2,14 @@ package pack12;
 
 public class Class12 {
 
+	public int r(){
+		System.err.println("r...");
+		return 0;
+	}
 	
 	
+	public int r1(){
+		System.err.println("r1");
+		return 0;
+	}
 }
